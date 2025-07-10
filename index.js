8ae46e7e56d6e449eb16762462c2c0cf3a756a7a
valorite2 <tudylanore2005@gmail.com>
2025-07-10 04:11:27 -0300

    Agregar servidor Express con rutas para entidad productos

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-console.log("soy Dylan Ore y este es el trabajo de Node.js para PDI (La mejor materia)");
-import chalk from "chalk";
-import { format } from "date-fns";
+import express from "express";
 
-console.log(chalk.green("Hola, soy Dylan Ore"));
+const app = express();
+const port = 3000;
 
-const hoy = new Date();
-console.log(chalk.blue(format(hoy, "10/06/2025")));
+app.use(express.json());
 
-const fechaEntrega = new Date(2025, 6, 15);
-if (hoy <= fechaEntrega) {
-  console.log(chalk.yellow(format(fechaEntrega, "15/06/2025")));
-} else {
-  console.log(chalk.red(format(fechaEntrega, "15/06/2025")));
-}
+app.get("/", (req, res) => {
+  res.json({ mensaje: "Bienvenido a la API de boosme" });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
+// Rutas para entidad "habitos"
+
+// GET para todos los productos
+app.get("/habitos", (req, res) => {
+    res.json({ mensaje: "Esta es la ruta GET de mi entidad habitos" });
+  });
+  
+  // POST para crear un producto
+  app.post("/habitos", (req, res) => {
+    res.json({ mensaje: "Esta es la ruta POST de mi entidad habitos" });
+  });
+  
+  // PUT para actualizar un producto
+  app.put("/habitos", (req, res) => {
+    res.json({ mensaje: "Esta es la ruta PUT de mi entidad habitos" });
+  });
+  
+  // DELETE para eliminar un producto
+  app.delete("/habitos", (req, res) => {
+    res.json({ mensaje: "Esta es la ruta DELETE de mi entidad habitos" });
+  });
+  
