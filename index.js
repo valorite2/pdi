05e2437f0f236fb06ae844ec17049cc2589265c9
valorite2 <tudylanore2005@gmail.com>
2025-07-10 03:53:48 -0300

    Agregadas dependencias chalk y date-fns y funcionalidades

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("soy Dylan Ore y este es el trabajo de Node.js para PDI (La mejor materia)");
+import chalk from "chalk";
+import { format } from "date-fns";
+
+console.log(chalk.green("Hola, soy Dylan Ore"));
+
+const hoy = new Date();
+console.log(chalk.blue(format(hoy, "10/06/2025")));
+
+const fechaEntrega = new Date(2025, 6, 15);
+if (hoy <= fechaEntrega) {
+  console.log(chalk.yellow(format(fechaEntrega, "15/06/2025")));
+} else {
+  console.log(chalk.red(format(fechaEntrega, "15/06/2025")));
+}
